680af658e05ca66d5a3e548ac182ddd49bcfe367
Eluisa <eluisadalmagro@gmail.com>
2024-10-02 21:15:03 -0300

    Add files via upload

---
+++ ExemploWhile/src/Main.java
@@ -0,0 +1,26 @@
+public static void main(String[] args) {
+
+    String nome = "PROVA_UNIPAR";
+
+    int MeuVetor
+    MeuVetor = (3);
+    MeuVetor = (9);
+    MeuVetor = (8);
+    MeuVetor = (10);
+    MeuVetor = (5);
+
+    int("i = 17.5; i < 0; i++");
+  MeuVetor = >3;
+    MeuVetor = >= 8;
+    MeuVetor = < 5;
+    MeuVetor = >= 10;
+    MeuVetor = >= 9;
+    String ("Nota que foi inserida acabou ocorrendo um erro");
+
+    String = "Concurso de Talentos";
+            MeuVetor = "taffe 80";
+            MeuVetor = "rodrigo 70";
+            MeuVetor = "lucas 60";
+            MeuVetor = "alessandro 50"}
+     int("i = 4; i < 4; i++")
+     String (" o ganhador foi taffe")
